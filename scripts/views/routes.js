10f308dbf89cb7507c27f639f2cdb0dcca5416d0
Koko Kassa <kowserk7@gmail.com>
2017-12-30 18:11:22 -0800

    fixed the routing problem

--- scripts/views/routes.js
+++ scripts/views/routes.js
@@ -6,7 +6,9 @@ page('/calendar', ctx => app.foodView.initWeekView(ctx));
 
 page('/recipeform', ctx => app.foodView.initRecipeList(ctx));
 
-page('/recipeupdate'),( ctx,next) => app.Food.fetchOne(ctx, next),ctx => app.foodView.initUpdate(ctx);
+page('/recipeupdate', (ctx,next) => app.Food.fetchOne(ctx, next),ctx => app.foodView.initUpdate(ctx));
+
+page();
 
 
 
