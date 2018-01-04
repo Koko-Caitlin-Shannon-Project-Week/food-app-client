bbc97cc6b6b1c804412d81b143e9e9ef0a24059a
Koko Kassa <kowserk7@gmail.com>
2018-01-04 09:37:35 -0800

    adding

--- scripts/models/food.js
+++ scripts/models/food.js
@@ -32,7 +32,8 @@ var __API_URL__ = 'https://cool-food.herokuapp.com';
     $.ajax({
       url: `${__API_URL__}/api/v1/recipes/${Food.currentUserID}/${day}`,
       method: 'PUT',
-      data: JSON.stringify(Food.recipeList.hits[recNum].recipe)
+      contentType: 'json',
+      data: Food.recipeList.hits[recNum].recipe,
     })
     .then (()=> page('/calendar'));
   }
@@ -132,7 +133,7 @@ var __API_URL__ = 'https://cool-food.herokuapp.com';
       page('/calendar');
     } else {
         $.post(`${__API_URL__}/api/v1/users`, {username: $('#user').val(), password: $('#password').val()})
-        .then(() => page('/calander'));
+        .then(() => page('/calendar'));
     }
     // page('/calendar');
 }
