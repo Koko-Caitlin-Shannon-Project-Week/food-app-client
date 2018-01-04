099d9129d4e24b92cef36593179910c607e1a8b5
Koko Kassa <kowserk7@gmail.com>
2018-01-04 10:58:19 -0800

    add css changes

--- scripts/views/routes.js
+++ scripts/views/routes.js
@@ -2,6 +2,8 @@
 
 page('/', (ctx,next)=>app.Food.fetchUsers(app.foodView.initLoginPage));
 
+page('/aboutus', app.foodView.initAboutUs);
+
 page('/calendar', app.foodView.initWeekView);
 
 page('/recipeform', ctx => app.foodView.initRecipeList(ctx));
@@ -19,7 +21,6 @@ page('/recipeformfri', ctx => app.foodView.initFormView('friday'));
 page('/recipeformsat', ctx => app.foodView.initFormView('saturday'));
 
 page('/recipeformsun', ctx => app.foodView.initFormView('sunday'));
-
 
 page('/recipeupdate', (ctx,next) => app.Food.getRecipes(ctx, next),ctx => app.foodView.initUpdate(ctx));
 
